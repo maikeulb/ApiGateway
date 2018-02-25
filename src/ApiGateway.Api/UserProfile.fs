9d6f52bd00833139b1a199fad625338ff783c3ad
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-24 21:51:12 -0500

    complete function to get value from HttpResponse

--- src/ApiGateway.Api/UserProfile.fs
+++ src/ApiGateway.Api/UserProfile.fs
@@ -1,16 +1,16 @@
-module GitHub
+module UserProfile
 
 open Http
 open FSharp.Data
 
-type User = JsonProvider<"user.json">
+type UserProfile = JsonProvider<"user.json">
 type UserPosts = JsonProvider<"posts.json">
 
 type Profile = {
     Name : string
     AvatarUrl : string
-    PopularRepositories : Repository seq
-} and Repository = {
+    PopularPostings : Posting seq
+} and Posting = {
     Name : string
     Stars : int
     Languages : string[]
@@ -18,8 +18,43 @@ type Profile = {
 
 let host = "https://api.github.com"
 let userUrl = sprintf "%s/users/%s" host
-let reposUrl = sprintf "%s/users/%s/repos" host
-let languagesUrl repoName userName = sprintf "%s/repos/%s/%s/languages" host userName repoName
+let postsUrl = sprintf "%s/users/%s/posts" host
+let languagesUrl postName userName = sprintf "%s/posts/%s/%s/languages" host userName postName
 
-let parseUser = GitHubUser.Parse
-let parseUserRepos = GitHubUserRepos.Parse
+let parseUser = UserProfile.Parse
+let parseUserPosts = UserPosts.Parse
+
+let popularPosts (posts : GitHubUserPosts.Root []) =
+    
+    let ownPosts = posts |> Array.filter (fun post -> not post.Fork)
+    let takeCount = if ownPosts.Length > 3 then 3 else posts.Length
+
+    ownPosts
+    |> Array.sortBy (fun r -> -r.StargazersCount)
+    |> Array.toSeq
+    |> Seq.take takeCount
+    |> Seq.toArray
+
+let parseLanguages languagesJson =
+    languagesJson
+    |> JsonValue.Parse
+    |> JsonExtensions.Properties
+    |> Array.map fst
+    
+let languageResponseToPostWithLanguages (post : UserPosts.Root) = function
+    |Ok(l) -> {Name = post.Name; Languages = (parseLanguages l); Stars = post.StargazersCount}
+    |_ -> {Name = post.Name; Languages = Array.empty; Stars = post.StargazersCount}
+
+let postsResponseToPopularPosts = function
+    |Ok(r) -> r |> parseUserPosts |> popularPosts
+    |_ -> [||]
+
+let toProfile  = function
+    |Ok(u), posts -> 
+        let user = parseUser u
+        {Name = user.Name; PopularPostings = posts; AvatarUrl = user.AvatarUrl} |> Some
+    | _ -> None
+
+
+
+
